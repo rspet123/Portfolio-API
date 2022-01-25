9ffcfd3bba92fc2290ad9de0bba55522cb3bffa3
R S <rspet123@gmail.com>
2022-01-25 02:23:46 -0500

    Add files via upload

--- PortfolioAPI/app/data_loader.py
+++ PortfolioAPI/app/data_loader.py
@@ -4,52 +4,54 @@ import os
 class DataLoader:
     """Set of functions to help loading and unloading data"""
     dirname = os.path.dirname(__file__)
-    recipe_path = (dirname+"\\recipes.json")
-
-    #our overloaded constructor, to make sure we can do other stuff, not just recipes
+    resume_path = (dirname+"\\resume.json")
 
 
     @classmethod
-    def load(cls):
+    def load(cls, resume_class: str):
         """Loads JSON file to a dict"""
-        recipe_dict = {}
-        with open(cls.recipe_path,"r") as data_file:
-            recipe_list = json.load(data_file).get("recipes")
-        #It gives us a list, which is okay
-        #but we kind of want it to be faster, and we want to be able to find
-        #recipies quickly, without having to iterate through the list
-        #So I'll turn it into a dict
-        for recipe in recipe_list:
-            recipe_dict[recipe["name"]] = {"ingredients":recipe["ingredients"],
-                                           "instructions":recipe["instructions"]}
-        return recipe_dict
+        resume_dict = {}
+        #Get work Experience
+        with open(cls.resume_path,"r") as data_file:
+            try:
+                resume_list = json.load(data_file).get(resume_class)
+            except Exception:
+                print("Error")
+                raise Exception("Whoops")
+        for data in resume_list:
+            resume_dict[data["name"]] = data
+        return resume_dict
 
     # Simply updates the recipe on the dict, but does not *save* it
     # or write it to the disk
     @classmethod
-    def update_recipe(cls, recipe: dict, updated_recipe: dict):
-        """Update Recipe"""
-        for key, val in updated_recipe.items():
-            recipe[key] = val
+    def update_resume(cls, resume_item: dict, updated_item: dict):
+        """Update Resume"""
+        for key, val in updated_item.items():
+            resume_item[key] = val
 
     # Helper method to allow us to turn the dict that we made when loading
     # back into a list, to allow us to save it on the disk
     # exactly how it was
     @staticmethod
-    def recipe_dict_to_list(recipe_dict: dict):
+    def dict_to_list(resume_dict: dict):
         """Helper Method, dict -> List"""
-        recipe_list =[]
-        for recipe_name in recipe_dict.keys():
-            recipe_list.append({"name":recipe_name,
-             "ingredients":recipe_dict[recipe_name]["ingredients"],
-             "instructions":recipe_dict[recipe_name]["instructions"]
-             })
-        return recipe_list
+        resume_list =[]
+        for name in resume_dict.keys():
+            resume_list.append(resume_dict[name])
+        return resume_list
     # we use this to write to disk
     @classmethod
-    def write(cls, recipe_dict: dict):
+    def write(cls, write_dict: dict):
         """Write Dict to disk"""
-        recipe_list = cls.recipe_dict_to_list(recipe_dict)
-        with open(cls.recipe_path, "w") as data_file:
-            json.dump({"recipes": recipe_list}, data_file, indent=2)
+        resume_list = cls.dict_to_list(write_dict)
+        with open("comments.json", "a") as data_file:
+            json.dump({"comment": resume_list}, data_file, indent=2)
+            
+    @classmethod
+    def add_comment(cls,comment,com_dir = dirname):
+         """Write comment to comment file"""
+         with open(com_dir+"\\comments.txt","a") as comment_text_file:
+            comment_text_file.writelines(comment)
+            comment_text_file.close()
             
--- PortfolioAPI/app/flask_routes.py
+++ PortfolioAPI/app/flask_routes.py
@@ -6,87 +6,40 @@ from . import app
 
 #Set Up Data stuff
 loader = DataLoader()
-
-recipes = loader.load()
 #GET
 
-@app.get("/recipes")
-def get_recipe_names(current_recipes = recipes):
-    """Gets all recipes"""
-    response = list(current_recipes.keys())
-    return {"recipeNames": response}
+@app.get("/resume")
+def resume():
+    """Gets whole resume"""
+    return {"resume": ["ContactInto","Coursework","Education","Languages","Projects","WorkExperience"]},200
 
 @app.get("/hello")
 def hello():
-    return 'hello world!',200
+    return {"resume": ["ContactInto","Coursework","Education","Languages","Projects","WorkExperience"]},200
     
-@app.get("/recipes/details/<name>")
-def get_recipe(name,current_recipes = recipes):
-    """Gets specific recipe"""
-    if name in current_recipes:
-        recipe = current_recipes[name]
-        return {"details":recipe}, 200
-    return "", 200
+@app.get("/resume/<resume_type>")
+def get_resume_type(resume_type,loader = loader):
+    """Gets specific type from resume"""
+    try:
+        resume = loader.load(resume_type)
+        return resume, 200
+    except Exception:
+        return "", 404
+
+
+@app.get("/comments")
+def get_comment_form():
+    """look into adding a new comment"""
+    return "You can POST a json here to send me a comment if you'd like :)"
 
 #POST
 
-@app.post("/recipes")
-def post_recipes(current_recipes = recipes,current_loader=loader):
-    """Post a new recipe"""
-    data = request.get_json(force=True)
-    # We make a backup here, incase something happens
-    # while we are writing the data to the JSON,
-    # in which case we will roll back to the origional state of the dict
-    # to avoid the two being out of sync
-    backup_recipes = current_recipes.copy()
-    if data.get("name") in current_recipes:
-        return {"error":"Recipe already exists"},400
-    try:
-        current_recipes[data.get("name")]={"ingredients":data.get("ingredients"),
-                                   "instructions":data.get("instructions")}
-        current_loader.write(current_recipes)
-    except Exception:
-        # we dont really care what the exception is
-        # we're just gonna roll it back, and return a 500
-        current_recipes = backup_recipes
-        return {"error": "Error writing, rolling back"}, 500
-    return "", 204
-
-#PUT
-
-@app.put("/recipes")
-def update_recipe(current_recipes = recipes,current_loader=loader):
-    """update an existing recipe"""
-    data = request.get_json(force=True)
-    # We make a backup here, incase something happens
-    # while we are writing the data to the JSON,
-    # in which case we will roll back to the origional state of the dict
-    # to avoid the two being out of sync
-    backup_recipes = current_recipes.copy()
-    if data.get("name") not in recipes:
-        return {"error": "Recipe doesn't exist"}, 404
-    try:
-        current_recipes[data.get("name")]["ingredients"] = data.get("ingredients")
-        current_recipes[data.get("name")]["instructions"] = data.get("instructions")
-        current_loader.write(current_recipes)
-    except Exception:
-        # we dont really care what the exception is
-        # we're just gonna roll it back, and return a 500
-        current_recipes = backup_recipes
-        return {"error": "Error writing, rolling back"}, 500
-
-    return "", 204
-
-#DELETE
-
-@app.delete("/recipes")
-def delete_recipe(current_recipes = recipes,current_loader=loader):
-    data = request.get_json(force=True)
-    if data.get("name") not in recipes:
-        return {"error": "Recipe doesn't exist"}, 404
-    try:
-        del current_recipes[data.get("name")]
-    except Exception:
-        return {"error": "Error writing, rolling back"}, 500
-    return {"deleted":  data.get("name")}, 200
+@app.post("/comments")
+def post_comment(current_loader=loader):
+    """Post a new Comment"""
+    comment = request.get_json(force=True)
+    comment["ip"] = request.remote_addr
+    loader.write(comment)
+    return "", 202
+    
     
